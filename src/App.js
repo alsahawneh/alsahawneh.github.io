eb0184f41a6471de146161e16642024ba61de377
alsahawneh <marina.alsahawneh@gmail.com>
2022-07-02 02:00:31 +0300

    Reactise the app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Test
+        </p>
+        <a
+          className="App-link"
+          href="https://alsahawneh.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Marina Alsahawneh
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
